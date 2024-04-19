21ce43c1ac01d11a37c3565cc13eb366aef34708
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-04-19 17:20:20 -0400

    Learned how to do querysets so moved inventory organization views

--- CaseManagement/urls.py
+++ CaseManagement/urls.py
@@ -5,6 +5,6 @@ from . import organization_views, casefile_views
 urlpatterns = [
     path("", organization_views.current_organization, name="currentOrganization"),
     path("<int:org_id>/", organization_views.get_organization_by_id, name="organizationById"),
-    path("inventory", casefile_views.inventory, name="inventory"),
+    path("inventory", organization_views.inventory, name="inventory"),
     path("casefile/<int:casefile_id>", casefile_views.get_casefile_by_id, name="casefile")
 ]
