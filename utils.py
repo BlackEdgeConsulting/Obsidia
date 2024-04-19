07af24cc984af580d2580c11560f1b9429135007
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-04-19 11:18:36 -0400

    Stubbed some utils but needs work

---
+++ utils.py
@@ -0,0 +1,35 @@
+import json
+import os
+from pymongo import MongoClient
+from obsidia.settings import DATABASES
+
+def get_test_database():
+    dbconfig = DATABASES["mongodb"]
+    return _get_db_handle_mongodb(
+        host=dbconfig["HOST"],
+        port=dbconfig["PORT"],
+        username=dbconfig["USER"],
+        password=dbconfig["PASSWORD"]
+    )
+
+def get_dev_database():
+    dbconfig = DATABASES["default"]
+
+def get_db_handle():
+    environment = os.environ.get("OBSIDIA_ENVIRONMENT")
+    if environment == "dev":
+        pass
+    elif environment == "test":
+        return get_test_database()
+    else:
+        raise NotImplementedError("Haven't implemented that database yet")
+
+
+def _get_db_handle_mongodb(host, port, username, password):
+    client = MongoClient(host=host,
+                      port=int(port),
+                      username=username,
+                      password=password
+                     )
+    db_handle = client['db_name']
+    return db_handle, client
