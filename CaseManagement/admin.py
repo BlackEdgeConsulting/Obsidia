3dcd97e429325aa2e3d57e63c7b76dfb43bdc048
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-07-13 14:11:45 -0400

    Registered the tagset and tag

--- CaseManagement/admin.py
+++ CaseManagement/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CaseFile, Organization, TargetOfInterest
+from .models import CaseFile, Organization, TargetOfInterest, Tag, TagSet
 
 admin.site.register(Organization)
 admin.site.register(CaseFile)
 admin.site.register(TargetOfInterest)
-# admin.site.register(Tag)
+admin.site.register(TagSet)
+admin.site.register(Tag)
