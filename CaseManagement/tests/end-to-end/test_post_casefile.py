f237149eb49a476cfd45f78ad2880bc2fe63bc9b
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-07-20 13:16:55 -0400

    Added more test cases

--- CaseManagement/tests/end-to-end/test_post_casefile.py
+++ CaseManagement/tests/end-to-end/test_post_casefile.py
@@ -2,7 +2,7 @@ import json
 from django.test import TestCase, Client
 from datetime import date, datetime
 from CaseManagement.DTOModels import DTOOrganization
-from CaseManagement.models import Organization
+from CaseManagement.models import Organization, CaseFile
 
 class OrganizationModelsPostTestCase(TestCase):
     range_of_entries = range(1,1000)
@@ -113,8 +113,132 @@ class OrganizationModelsPostTestCase(TestCase):
             self.assertEqual(response.content.decode("UTF-8"), "Created the new casefile!")
         
 
-    def test_casefile_POST_casefile_with_full_target(self):
-        pass
+    def test_casefile_POST_casefile_with_full_and_partial_target(self):
+        payloads = [
+            {
+                "name": "test with full target",
+                "payload": {
+                    "caseIdentifier": "ojdsf973rhiohifh",
+                    "organization": 1,
+                    "status": "ACTIVE",
+                    "tags": [
+                        {
+                            "key": "something",
+                            "value": "asdh76"
+                        },
+                        {
+                            "key": "somethingElse",
+                            "value": "ihsdf7893"
+                        }
+                    ],
+                    "targetOfInterest": {
+                        "firstName": "Abby",
+                        "middleNames": "Ethelbert",
+                        "lastName": "Luthwood",
+                        "fullName": "Ethelbert Luthwood",
+                        "additionalNames": "vestibulum rutrum rutrum neque aenean auctor gravida sem praesent id massa id",
+                        "dateOfBirth": "1977-09-28T07:44:46Z",
+                        "currentAddress": "39913 Charing Cross Point",
+                        "previousAddresses": [
+                            "39233 Charing Cross Point"
+                        ],
+                        "associatedAddresses": [
+                            "39913 Charing Cross Point",
+                            "39233 Charing Cross Point"
+                        ],
+                        "targetJustification": "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+                        "socialSecurityNumber": "114-41-3171",
+                        "driversLicenseNumber": "DL654321",
+                        "governmentIssueId": "ID987654",
+                        "additionalIdentifications": "tortor id nulla ultrices aliquet maecenas leo odio condimentum",
+                    }
+                },
+                "expect": {}
+            },
+            {
+                "name": "test w/ Partial Target",
+                "payload": {
+                    "caseIdentifier": "qwioklasjdo973",
+                    "organization": 1,
+                    "status": "ACTIVE",
+                    "tags": [
+                        {
+                            "key": "something",
+                            "value": "asdh76"
+                        }
+                    ],
+                    "targetOfInterest": {
+                        "firstName": "Abby",
+                        "middleNames": "Kary",
+                        "lastName": "Prowting",
+                        "previousAddresses": ["8065 Carberry Center"],
+                        "targetJustification": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+                        "additionalIdentifications": "accumsan felis"
+                    }
+                },
+                "expect": {}
+            }
+        ]
 
-    def test_Casefile_POST_casefile_with_partial_target(self):
-        pass
+        for each_payload in payloads:
+            response = self.client.post(
+                "/organization/casefile",
+                data=each_payload["payload"],
+                content_type="application/json",
+                HTTP_X_REQUESTED_WITH="XMLHttpRequest"
+            )
+            posted_casefile = CaseFile.objects.get(caseIdentifier=each_payload["payload"]["caseIdentifier"])
+            self.assertEqual(response.content.decode("UTF-8"), "Created the new casefile!")
+            self.assertEqual(response.status_code, 201)
+
+    def test_casefile_POST_casefile_with_no_correct_properties(self):
+        payloads = [
+            {
+                "name": "test with full target",
+                "payload": {
+                    "caseIdentifier": "ojdsf973rhiohifh",
+                    "organization": 1,
+                    "status": "ACTIVE",
+                    "tags": [
+                        {
+                            "key": "something",
+                            "value": "asdh76"
+                        },
+                        {
+                            "key": "somethingElse",
+                            "value": "ihsdf7893"
+                        }
+                    ],
+                    "targetOfInterest": {
+                        "FirStName": "Abby",
+                        "miDDlENames": "Ethelbert",
+                        "last": "Luthwood",
+                        "fUlMAne": "Ethelbert Luthwood",
+                        "adDDitionalNames": "vestibulum rutrum rutrum neque aenean auctor gravida sem praesent id massa id",
+                        "DoB": "1977-09-28T07:44:46Z",
+                        "curRent": "39913 Charing Cross Point",
+                        "previusAddresses": [
+                            "39233 Charing Cross Point"
+                        ],
+                        "associasastedAddresses": [
+                            "39913 Charing Cross Point",
+                            "39233 Charing Cross Point"
+                        ],
+                        "targetJustifasdffication": "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+                        "sociaa2aslSecurityNumber": "114-41-3171",
+                        "driversLidasdcenseNumber": "DL654321",
+                        "governmentIsfasdsueId": "ID987654",
+                        "additionaasdasfdflIdentifications": "tortor id nulla ultrices aliquet maecenas leo odio condimentum",
+                    }
+                },
+            }
+        ]
+
+        for each_payload in payloads:
+            response = self.client.post(
+                "/organization/casefile",
+                data=each_payload["payload"],
+                content_type="application/json",
+                HTTP_X_REQUESTED_WITH="XMLHttpRequest"
+            )
+            self.assertEqual(response.status_code, 400)
